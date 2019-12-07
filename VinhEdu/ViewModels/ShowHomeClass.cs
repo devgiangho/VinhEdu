5e1ae4ce39b9d199456a6325b874c20789bf1078
Kiên Đinh <kienmatu1@gmail.com>
2019-12-07 09:35:44 +0700

    add file thiếu

---
+++ VinhEdu/ViewModels/ShowHomeClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VinhEdu.ViewModels
+{
+    public class ShowHomeClass
+    {
+        public string ClassName { get; set; }
+        public int CountStudent { get; set; }
+        public int ClassID { get; set; }
+    }
+}
